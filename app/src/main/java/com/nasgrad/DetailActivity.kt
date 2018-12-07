d17fd5c821e30dc32d3f12e3c6a98bf25bf0e67b
Stanislav Ivanovic <stanislav.ivanovic@yahoo.com>
2018-12-07 18:18:16 +0100

    feat: Added report issue action opens Email client

--- app/src/main/java/com/nasgrad/DetailActivity.kt
+++ app/src/main/java/com/nasgrad/DetailActivity.kt
@@ -1,18 +1,38 @@
 package com.nasgrad
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Toast
 import com.nasgrad.MainActivity.Companion.ITEM_ID
 import com.nasgrad.nasGradApp.R
 import kotlinx.android.synthetic.main.activity_detail.*
 
-class DetailActivity : AppCompatActivity() {
+class DetailActivity : AppCompatActivity() , OnClickListener{
+    override fun onClick(view: View) {
+        if (view.id == reportIssue.id){
+            openEmailClint()
+        }
+    }
+
+    private fun openEmailClint(){
+        Toast.makeText(this,"Prijavi problem",Toast.LENGTH_SHORT).show()
+        val intent = Intent(Intent.ACTION_VIEW)
+        val data = Uri.parse("mailto:${resources.getString(R.string.email)}?subject=Prijavi Problem&body=OpisProblem" );
+        intent.data = data
+        startActivity(intent)
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
         val itemID = intent.getStringExtra(ITEM_ID)
         titleDetailsLabel.text = itemID
+        reportIssue.setOnClickListener(this)
     }
 
     override fun onBackPressed() {

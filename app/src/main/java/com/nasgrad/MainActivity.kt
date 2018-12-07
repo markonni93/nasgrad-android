70dd8caeb7f5c3a822452d925b5298cffdc723e4
Strahinja Vuckovic <strahinja.vuckovic94@gmail.com>
2018-12-07 13:53:46 +0100

    Merge develop intu current branch

--- app/src/main/java/com/nasgrad/MainActivity.kt
+++ app/src/main/java/com/nasgrad/MainActivity.kt
@@ -3,7 +3,6 @@ package com.nasgrad
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.nasgrad.nasGradApp.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {

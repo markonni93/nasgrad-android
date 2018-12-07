94067c227a6da13c194ad7c586f43f144e1eb861
Strahinja Vuckovic <strahinja.vuckovic94@gmail.com>
2018-12-07 18:40:56 +0100

    Add picture from list into detail screen

--- app/src/main/java/com/nasgrad/DetailActivity.kt
+++ app/src/main/java/com/nasgrad/DetailActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.view.View.OnClickListener
+import com.bumptech.glide.Glide
 import com.nasgrad.MainActivity.Companion.ITEM_ID
 import com.nasgrad.nasGradApp.R
 import kotlinx.android.synthetic.main.activity_detail.*
@@ -30,6 +31,7 @@ class DetailActivity : AppCompatActivity(), OnClickListener {
         setContentView(R.layout.activity_detail)
         val itemID = intent.getStringExtra(ITEM_ID)
         titleDetailsLabel.text = itemID
+        Glide.with(this).load("https://picsum.photos/100/100/?random").into(issuePicture)
         reportIssue.setOnClickListener(this)
     }
 

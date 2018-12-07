205f3df267f0db0a79b2d9110e1c3671e1ecaa5a
Strahinja Vuckovic <strahinja.vuckovic94@gmail.com>
2018-12-07 18:37:20 +0100

    Share picture between list and detail screen

--- app/src/main/java/com/nasgrad/DetailActivity.kt
+++ app/src/main/java/com/nasgrad/DetailActivity.kt
@@ -2,6 +2,7 @@ package com.nasgrad
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.nasgrad.MainActivity.Companion.ITEM_ID
 import com.nasgrad.nasGradApp.R
 import kotlinx.android.synthetic.main.activity_detail.*
@@ -13,6 +14,8 @@ class DetailActivity : AppCompatActivity() {
         setContentView(R.layout.activity_detail)
         val itemID = intent.getStringExtra(ITEM_ID)
         titleDetailsLabel.text = itemID
+        Glide.with(this).load("https://picsum.photos/100/100/?random").into(issuePicture)
+
     }
 
     override fun onBackPressed() {

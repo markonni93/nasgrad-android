29a236000008b779455fb73e5a3267cced4c6529
Strahinja Vuckovic <strahinja.vuckovic94@gmail.com>
2018-12-07 15:40:55 +0100

    Create list item, need to implement glide

--- app/src/main/java/com/nasgrad/MainActivity.kt
+++ app/src/main/java/com/nasgrad/MainActivity.kt
@@ -3,7 +3,12 @@ package com.nasgrad
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.nasgrad.adapter.IssueAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import com.nasgrad.nasGradApp.R
+import kotlinx.android.synthetic.main.issue_list_item.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,5 +19,19 @@ class MainActivity : AppCompatActivity() {
         fab.setOnClickListener {
             startActivity(Intent(this@MainActivity, CreateIssueActivity::class.java))
         }
+        setIssueListAdapter()
+    }
+
+    fun setIssueListAdapter() {
+        val layoutManager = LinearLayoutManager(this)
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+        rvIssueList.layoutManager = layoutManager
+
+        val issueAdapter = IssueAdapter(this, CaneModel.Cane)
+//        Glide.with(this).load("https://picsum.photos/100/100/?random").into(ivIssueImage)
+//        layoutManager.addView(ivIssueImage)
+        rvIssueList.adapter = issueAdapter
+
+
     }
 }

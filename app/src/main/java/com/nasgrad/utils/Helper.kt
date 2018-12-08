5a3ccbe0d45b6b3bbc669eccc4a2733926a32ce0
Stanislav Ivanovic <stanislav.ivanovic@yahoo.com>
2018-12-08 03:25:12 +0100

    feat: Added function decoding and encode a picture

--- app/src/main/java/com/nasgrad/utils/Helper.kt
+++ app/src/main/java/com/nasgrad/utils/Helper.kt
@@ -1,6 +1,11 @@
 package com.nasgrad.utils
 
+import android.graphics.Bitmap
 import java.util.*
+import android.graphics.BitmapFactory
+import android.util.Base64
+import java.io.ByteArrayOutputStream
+
 
 class Helper {
     companion object {
@@ -8,6 +13,17 @@ class Helper {
 
         fun randomGUID(): String {
             return UUID.randomUUID().toString()
+        }
+
+        fun decodePicturePreview (picturePreview:String): Bitmap {
+            val decodedString = Base64.decode(picturePreview,Base64.DEFAULT)
+            return BitmapFactory.decodeByteArray(decodedString, 0, decodedString.size)
+        }
+
+        fun encodePicturePreview (picturePreview: Bitmap) : String{
+            val stream = ByteArrayOutputStream()
+            picturePreview.compress(Bitmap.CompressFormat.JPEG, 90, stream)
+            return stream.toByteArray().toString()
         }
     }
 }
